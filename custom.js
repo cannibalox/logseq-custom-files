012bc56741c0b40738574d7e60c7f7a8887bd459
cannibalox <cannibalox@users.noreply.github.com>
2022-05-17 15:15:43 +0200

    add twitter embed

--- custom.js
+++ custom.js
@@ -1,105 +1,162 @@
 // common =================================================================
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-// throttle MutationObserver 
-// from https://stackoverflow.com/a/52868150
-  const throttle = (func, limit) => {
-    let inThrottle;
-    return (...args) => {
-      if (!inThrottle) {
-        func(...args);
-        inThrottle = setTimeout(() => (inThrottle = false), limit);
-      }
-    };
-  };
+  MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
   const watchTarget = document.getElementById("app-container"); 
+  // throttle MutationObserver 
+  // from https://stackoverflow.com/a/52868150
+    const throttle = (func, limit) => {
+      let inThrottle;
+      return (...args) => {
+        if (!inThrottle) {
+          func(...args);
+          inThrottle = setTimeout(() => (inThrottle = false), limit);
+        }
+      };
+    };
+// ================================== END COMMON
+
 // query table resizer ============================================== 
 // source : https://htmldom.dev/resize-columns-of-a-table/   
-    console.log("========= query table resizer v20220312 ============");
+  console.log("========= query table resizer v20220312 ============");
 
-    const createResizableColumn = function (col, resizer) {
-      // Track the current position of mouse
-      let x = 0;
-      let w = 0;
+  const createResizableColumn = function (col, resizer) {
+    // Track the current position of mouse
+    let x = 0;
+    let w = 0;
 
-      const mouseDownHandler = function (e) {
-        // Get the current mouse position
-        x = e.clientX;
+    const mouseDownHandler = function (e) {
+      // Get the current mouse position
+      x = e.clientX;
 
-        // Calculate the current width of column
-        const styles = window.getComputedStyle(col);
-        w = parseInt(styles.width, 10);
+      // Calculate the current width of column
+      const styles = window.getComputedStyle(col);
+      w = parseInt(styles.width, 10);
 
-        // Attach listeners for document's events
-        document.addEventListener("mousemove", mouseMoveHandler);
-        document.addEventListener("mouseup", mouseUpHandler);
-      };
-
-      const mouseMoveHandler = function (e) {
-        // Determine how far the mouse has been moved
-        const dx = e.clientX - x;
-        // Update the width of column
-        col.style.width = `${w + dx}px`;
-      };
-
-      // When user releases the mouse, remove the existing event listeners
-      const mouseUpHandler = function () {
-        document.removeEventListener("mousemove", mouseMoveHandler);
-        document.removeEventListener("mouseup", mouseUpHandler);
-      };
-      resizer.addEventListener("mousedown", mouseDownHandler);
+      // Attach listeners for document's events
+      document.addEventListener("mousemove", mouseMoveHandler);
+      document.addEventListener("mouseup", mouseUpHandler);
     };
 
-    const updateTables = function () {
-      // Query the table
-      const table = document.querySelectorAll(".table-auto:not(.table-resizable)");
-      for (let i = 0; i < table.length; i++) {
-        // Query all headers1
-        const cols = table[i].querySelectorAll("thead tr > th.whitespace-nowrap");
-        // Loop ver them
-        Array.from(cols).forEach((col) => {
-          // Create a resizer element
-          const resizer = document.createElement("div");
-          resizer.classList.add("query-table-resizer");
-          table[i].classList.add("table-resizable");
-          console.info("-- injected div.query-table-resizer --");
-          // Add a resizer element to the column
-          col.appendChild(resizer);
-          createResizableColumn(col, resizer);
-        });
-      }
+    const mouseMoveHandler = function (e) {
+      // Determine how far the mouse has been moved
+      const dx = e.clientX - x;
+      // Update the width of column
+      col.style.width = `${w + dx}px`;
     };
 
-    const updateTablesThrottled = throttle(updateTables, 1000);
-    const obsTable = new MutationObserver(updateTablesThrottled);
-    obsTable.observe(watchTarget, {
-      subtree: true,
-      childList: true,
-    }); 
+    // When user releases the mouse, remove the existing event listeners
+    const mouseUpHandler = function () {
+      document.removeEventListener("mousemove", mouseMoveHandler);
+      document.removeEventListener("mouseup", mouseUpHandler);
+    };
+    resizer.addEventListener("mousedown", mouseDownHandler);
+  };
+
+  const updateTables = function () {
+    // Query the table
+    const table = document.querySelectorAll(".table-auto:not(.table-resizable)");
+    for (let i = 0; i < table.length; i++) {
+      // Query all headers1
+      const cols = table[i].querySelectorAll("thead tr > th.whitespace-nowrap");
+      // Loop ver them
+      Array.from(cols).forEach((col) => {
+        // Create a resizer element
+        const resizer = document.createElement("div");
+        resizer.classList.add("query-table-resizer");
+        table[i].classList.add("table-resizable");
+        console.info("-- injected div.query-table-resizer --");
+        // Add a resizer element to the column
+        col.appendChild(resizer);
+        createResizableColumn(col, resizer);
+      });
+    }
+  };
+
+  const updateTablesThrottled = throttle(updateTables, 1000);
+  const obsTable = new MutationObserver(updateTablesThrottled);
+  obsTable.observe(watchTarget, {
+    subtree: true,
+    childList: true,
+  }); 
 // ====================================================== query table resizer
 
 // namespace prefixes collapser =============================================
+  function hideNamespace() {
+    console.info("====== LS HIDE NAMESPACE v20220314 =====");
+    let nmsp = document.querySelectorAll(
+      'a.page-ref[data-ref*="/"]:not(.hidden-namespace)'
+    );
+    for (var i = 0; i < nmsp.length; i++) {
+      if (nmsp[i].innerText.indexOf("/") !== -1) {
+        nmsp[i].innerHTML =
+          "<span style='color:rgb(133, 211, 81)'>..</span>" +
+          nmsp[i].innerText.substring(nmsp[i].innerText.lastIndexOf("/"));
+        nmsp[i].classList.add("hidden-namespace"); 
+        //console.info(" namespace off ==> " + nmsp[i].innerText);
+      }
+    }
+  }
 
-    function hideNamespace() {
-      console.info("====== LS HIDE NAMESPACE v20220314 =====");
-      let nmsp = document.querySelectorAll(
-        'a.page-ref[data-ref*="/"]:not(.hidden-namespace)'
-      );
-      for (var i = 0; i < nmsp.length; i++) {
-        if (nmsp[i].innerText.indexOf("/") !== -1) {
-          nmsp[i].innerHTML =
-            "<span style='color:rgb(133, 211, 81)'>..</span>" +
-            nmsp[i].innerText.substring(nmsp[i].innerText.lastIndexOf("/"));
-          nmsp[i].classList.add("hidden-namespace"); 
-          //console.info(" namespace off ==> " + nmsp[i].innerText);
-        }
+  const updateHideNamespace = throttle(hideNamespace, 1000);
+  const obsNamespace = new MutationObserver(updateHideNamespace);
+  obsNamespace.observe(watchTarget, {
+    subtree: true,
+    attributes: true,
+  });
+//===================================== end of namespace prefixes collapser 
+
+// ====== LS-TWITTER-EMBED =============================================
+  console.info('====== LS-TWITTER-EMBED ======');
+  // add twitter script and meta tags to head
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.src = "https://platform.twitter.com/widgets.js";
+  s.async = true;
+  var m = document.createElement("meta");
+  m.name = "twitter:widgets:theme";
+  m.content = "dark";
+  document.head.append(s, m);
+
+  function embedTwitter() {
+    let isTweet = document.querySelectorAll(
+      "a.external-link[href^='https://twitter.com"
+    );
+    for (let i = 0; i < isTweet.length; i++) {
+      if (isTweet[i].children[0] === undefined) {
+        var requestUrl =
+          "https://publish.twitter.com/oembed?omit_script=1&url=" +
+          isTweet[i].href + "&limit=8&theme=dark&maxwidth=550&maxheight=600";
+        var oReq = new XMLHttpRequest();
+        oReq.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            console.log(
+              'requestUrl      : ', requestUrl,
+              '\noReq.response : ', oReq.response,
+              '\ndata          : ', data,
+              '\ndata.html     : ', data.html
+              );
+              insertResponse(data, i);
+            }
+          }
+        oReq.open("GET", requestUrl, true);
+        oReq.send();
       }
     }
 
-    const updateHideNamespace = throttle(hideNamespace, 1000);
-    const obsNamespace = new MutationObserver(updateHideNamespace);
-    obsNamespace.observe(watchTarget, {
-      subtree: true,
-      attributes: true,
-    });
-//===================================== end of namespace prefixes collapser 
+    function insertResponse(data, i) {
+      var insTw = document.createElement("div");
+      insTw.className = "twembed";
+      insTw.innerHTML = data.html;
+      isTweet[i].appendChild(insTw);
+      console.log("embedding Tweets...");
+      twttr.widgets.load();
+    }
+  };
+  const embTwthrottled = throttle(embedTwitter, 1000);
+  const embTw = new MutationObserver(embTwthrottled);
+  const embTwTarget = document.getElementById('main-container');
+  embTw.observe(embTwTarget, {
+    subtree: true,
+    childList: true,
+  });
+// =============================================== end of twitter embed =
